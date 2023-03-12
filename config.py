12a76277029d103d3b39c5f5d84d26372a104948
Thebosh123 <99019809+Thebosh123@users.noreply.github.com>
2023-03-13 01:37:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv("config.env")
 
-API_ID = int(getenv("API_ID", 0))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OPENAI_API = getenv("OPENAI_API", "") # get api key : https://platform.openai.com/account/api-keys
+API_ID = int(getenv("API ID", 19132497))
+API_HASH = getenv("API_HASH", "55a02a263088db38e00db9287ce5d4e3")
+BOT_TOKEN = getenv("BOT_TOKEN", "6019411469:AAFSN9ANdv9FBTz9lxYbEvevPleqiSu1LvA")
+OPENAI_API = getenv("OPENAI_API", "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") # get api key : https://platform.openai.com/account/api-keys
